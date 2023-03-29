45de91abb906f5ff67bf1dcc26e8b4dcf978f898
jdeon <jd.eymann@gmail.com>
2023-03-29 17:56:36 +0200

    feat: limit number of particules

--- script/particuleEmiter.js
+++ script/particuleEmiter.js
@@ -2,19 +2,22 @@ import {Particule, ParticuleTemplate} from "./model.js"
 
 export default class ParticuleEmitter { 
     
-    static emitParticules(positionSpawning, particuleVelocity, particuleSize, particuleLifetime, particuleFrequence){
+    static emitParticules(maxParticules, positionSpawning, particuleVelocity, particuleSize, particuleLifetime, particuleFrequence){
         const particuleTexture = PIXI.Texture.from('/modules/particule-fx/particule.png');
         const particuleTemplate = new ParticuleTemplate(positionSpawning, particuleVelocity, particuleSize, particuleLifetime, particuleTexture);
+        const particuleEmitter = new ParticuleEmitter(particuleTemplate, particuleFrequence, maxParticules);
 
         // Listen for animate update
         canvas.app.ticker.add(particuleEmitter.manageSpawning.bind(particuleEmitter))
     }
 
 
+    constructor(particuleTemplate, particuleFrequence, maxParticules){
         this.spawnedEnable = true;
         this.particules = [];
         this.particuleTemplate = particuleTemplate;
         this.particuleFrequence = particuleFrequence;
+        this.maxParticules = maxParticules
     }
 
     manageSpawning(){
@@ -41,6 +44,7 @@ export default class ParticuleEmitter {
             }
         }
 
+        if (this.spawnedEnable && this.particules.length < this.maxParticules){
             let sprite = new PIXI.Sprite(this.particuleTemplate.particuleTexture)
             sprite.x = this.particuleTemplate.positionSpawning.x;
             sprite.y = this.particuleTemplate.positionSpawning.y;
