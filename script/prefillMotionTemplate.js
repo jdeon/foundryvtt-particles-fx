55be4a37f913aa6c44d758e267b622986f950310
jdeon <jd.eymann@gmail.com>
2023-04-08 15:29:18 +0200

    feat explosionTemplate

--- script/prefillMotionTemplate.js
+++ script/prefillMotionTemplate.js
@@ -24,26 +24,19 @@ export const defaultMotionTemplate = {
 }
 
 export const explosionMotionTemplate = {
-    spawningFrequence: 3, 
-    maxParticules: 100,
-    positionSpawning: {x:0,y:0},
-    particuleLifetime: [1000,1500],
-    particuleVelocityStart: 200,
-    particuleVelocityEnd: 50,
+    emissionDuration : 250,
+    spawningFrequence: .5, 
+    maxParticules: 10000,
+    particuleLifetime: 500,
+    particuleVelocityStart: '400_500',
+    particuleVelocityEnd: '0_25',
     particuleAngleStart: '0_360',
-    particuleAngleEnd: 0,
-    particuleRadiusStart: 100,
-    particuleRadiusEnd: 50,
     particuleSizeStart: 10,
-    particuleSizeEnd: '10_25',
+    particuleSizeEnd: '25_50',
     particuleColorStart:new Vector3(250, 250, 50),
     particuleColorEnd:new Vector3(250, '50_100', 0),
     alphaStart:1,
-    alphaEnd:0,
-    vibrationAmplitudeStart: 0,
-    vibrationAmplitudeEnd: 0,
-    vibrationFrequencyStart: 0,
-    vibrationFrequencyEnd: 0,
+    alphaEnd:.75
 }
 
 export const motionTemplateDictionnary = {
