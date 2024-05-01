fff293921be6c5002b41a3a235e2479f979fbefb
jdeon <jd.eymann@gmail.com>
2024-05-01 20:42:11 +0200

    fix: sameStartKey  without getValue and type fix

--- script/object/particle.js
+++ script/object/particle.js
@@ -20,19 +20,19 @@ export class Particle {
         this.sprite = sprite;                                                               //PIXI.Sprite
         this.positionVibrationLess = {x : sprite.x, y : sprite.y};  
         this.remainingTime = particleLifetime;                                             //Number
-        this.particleLifetime = particleLifetime;                                         //Number
-        this.sizeStart = sizeStart;                                                         //Vector3
-        this.sizeEnd = sizeEnd === sameStartKey ? sizeStart : sizeEnd;                      //Vector3
+        this.particleLifetime = particleLifetime;                                          //Number
+        this.sizeStart = sizeStart;                                                        //ParticuleInput<Vector3>
+        this.sizeEnd = sizeEnd.getValue() === sameStartKey ? sizeStart : sizeEnd;                     //ParticuleInput<Vector3>
         this.particleRotationStart = particleRotationStart;                 
-        this.particleRotationEnd = particleRotationEnd === sameStartKey ? particleRotationStart : particleRotationEnd;     
-        this.colorStart = colorStart;                                                       //Vector3
-        this.colorEnd = colorEnd  === sameStartKey ? colorStart : colorEnd;                 //Vector3
-        this.alphaStart = alphaStart;                                                       //Number
-        this.alphaEnd = alphaEnd  === sameStartKey ? alphaStart : alphaEnd;                 //Number
+        this.particleRotationEnd = particleRotationEnd.getValue() === sameStartKey ? particleRotationStart : particleRotationEnd;     
+        this.colorStart = colorStart;                                                       //ParticuleInput<Vector3>
+        this.colorEnd = colorEnd.getValue()  === sameStartKey ? colorStart : colorEnd;                 //ParticuleInput<Vector3>
+        this.alphaStart = alphaStart;                                                       //ParticuleInput<Number>
+        this.alphaEnd = alphaEnd.getValue()  === sameStartKey ? alphaStart : alphaEnd;                 //ParticuleInput<Number>
         this.vibrationAmplitudeStart = vibrationAmplitudeStart
-        this.vibrationAmplitudeEnd = vibrationAmplitudeEnd  === sameStartKey ? vibrationAmplitudeStart : vibrationAmplitudeEnd
+        this.vibrationAmplitudeEnd = vibrationAmplitudeEnd.getValue()  === sameStartKey ? vibrationAmplitudeStart : vibrationAmplitudeEnd
         this.vibrationFrequencyStart = vibrationFrequencyStart
-        this.vibrationFrequencyEnd = vibrationFrequencyEnd  === sameStartKey ? vibrationFrequencyStart : vibrationFrequencyEnd
+        this.vibrationFrequencyEnd = vibrationFrequencyEnd.getValue()  === sameStartKey ? vibrationFrequencyStart : vibrationFrequencyEnd
     }
 
     manageLifetime(dt){
@@ -75,10 +75,10 @@ export class SprayingParticle  extends Particle {
         super(advancedVariables, sprite, particleLifetime, sizeStart, sizeEnd, particleRotationStart, particleRotationEnd, colorStart, colorEnd, alphaStart, alphaEnd,vibrationAmplitudeStart, vibrationAmplitudeEnd, vibrationFrequencyStart, vibrationFrequencyEnd)
 
         this.target = target;
-        this.velocityStart = velocityStart;                                                 //Number      
-        this.velocityEnd = velocityEnd === sameStartKey ? velocityStart : velocityEnd;      //Number
-        this.angleStart = angleStart;                                                       //Number      
-        this.angleEnd = angleEnd === sameStartKey ? angleStart : angleEnd;                  //Number
+        this.velocityStart = velocityStart;                                                             //ParticleInput<Number>      
+        this.velocityEnd = velocityEnd.getValue() === sameStartKey ? velocityStart : velocityEnd;      //ParticleInput<Number>
+        this.angleStart = angleStart;                                                                   //ParticleInput<Number>     
+        this.angleEnd = angleEnd.getValue() === sameStartKey ? angleStart : angleEnd;                  //ParticleInput<Number>
     }
 
     manageLifetime(dt){
@@ -115,11 +115,11 @@ export class GravitingParticle  extends Particle {
         super(advancedVariables, sprite, particleLifetime, sizeStart, sizeEnd, particleRotationStart, particleRotationEnd, colorStart, colorEnd, alphaStart, alphaEnd, vibrationAmplitudeStart, vibrationAmplitudeEnd, vibrationFrequencyStart, vibrationFrequencyEnd)
 
         this.source = source
-        this.angle = angleStart.getValue()                                          //Number 
-        this.angularVelocityStart = angularVelocityStart;                           //Number      
-        this.angularVelocityEnd = angularVelocityEnd === sameStartKey ? angularVelocityStart : angularVelocityEnd;                               //Number
-        this.radiusStart = radiusStart;                                             //Number      
-        this.radiusEnd = radiusEnd === sameStartKey ? radiusStart : radiusEnd;                                                 //Number
+        this.angle = angleStart                                                     //Number 
+        this.angularVelocityStart = angularVelocityStart;                           //ParticleInput<Number>
+        this.angularVelocityEnd = angularVelocityEnd.getValue() === sameStartKey ? angularVelocityStart : angularVelocityEnd;                               //ParticleInput<Number>
+        this.radiusStart = radiusStart;                                             //ParticleInput<Number>
+        this.radiusEnd = radiusEnd.getValue() === sameStartKey ? radiusStart : radiusEnd;      //ParticleInput<Number>
     }
 
     manageLifetime(dt){
