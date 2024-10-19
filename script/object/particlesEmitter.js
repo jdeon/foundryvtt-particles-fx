132ee424af1119d9c54e41d608c4c95220ba2238
jdeon <jd.eymann@gmail.com>
2024-10-19 19:11:01 +0200

    feat: hide particles bellow objects with higher elevation (#106)
    * chore: refactor and use interface layer
    
    * fix: elevation source position
    
    * feat: add distance elevation directly on sprite
    
    * feat: add spirte on primary canvas
    
    * feat: sort primary canvas at each tick
    
    * chore: remove commented code
    
    * feat: particle layer is upper than other except weather
    
    * fix: get z value if case source has no elevation property but have z

--- script/object/particlesEmitter.js
+++ script/object/particlesEmitter.js
@@ -1,7 +1,7 @@
 import { motionTemplateDictionnary } from "../prefillMotionTemplate.js"
 import { colorTemplateDictionnary } from "../prefillColorTemplate.js"
 
-export default class ParticlesEmitter { 
+export default class ParticlesEmitter {
 
     static prefillMotionTemplate = motionTemplateDictionnary
     static prefillColorTemplate = colorTemplateDictionnary
@@ -11,18 +11,18 @@ export default class ParticlesEmitter {
     static _EMISSION_CANVAS
 
     static INIT_EMISSION_CANVAS = () => {
-        if(canvas.app.stage.rendered.environment.effects.moduleParticlesFx) {
-            canvas.app.stage.rendered.environment.effects.moduleParticlesFx.destroy()
+        if (canvas.app.stage.rendered.interface.moduleParticlesFx) {
+            canvas.app.stage.rendered.interface.moduleParticlesFx.destroy()
         }
-            
+
         const particleFxCanvas = new PIXI.Container()
-        canvas.app.stage.rendered.environment.effects.addChild(particleFxCanvas);
-        canvas.app.stage.rendered.environment.effects.moduleParticlesFx = particleFxCanvas
+        canvas.app.stage.rendered.interface.addChild(particleFxCanvas);
+        canvas.app.stage.rendered.interface.moduleParticlesFx = particleFxCanvas
         ParticlesEmitter._EMISSION_CANVAS = particleFxCanvas
     }
 
 
-    constructor(particleTemplate, particleFrequence, spawningNumber, maxParticles, emissionDuration, isGravitate){
+    constructor(particleTemplate, particleFrequence, spawningNumber, maxParticles, emissionDuration, isGravitate) {
         this.spawnedEnable = true;
         this.particles = [];
         this.particleTemplate = particleTemplate;
@@ -33,12 +33,12 @@ export default class ParticlesEmitter {
         this.isGravitate = isGravitate
         this.lastUpdate = Date.now();
 
-        if(!ParticlesEmitter._EMISSION_CANVAS){
+        if (!ParticlesEmitter._EMISSION_CANVAS) {
             ParticlesEmitter.INIT_EMISSION_CANVAS()
         }
     }
 
-    manageParticles(){
+    manageParticles() {
         let newDate = Date.now()
         const dt = newDate - this.lastUpdate
         this.lastUpdate = newDate
@@ -48,38 +48,40 @@ export default class ParticlesEmitter {
 
             particle.manageLifetime(dt)
 
-            if(particle.remainingTime <= 0){
+            if (particle.remainingTime <= 0) {
                 particle.sprite.destroy()
                 this.particles.splice(i, 1)
                 //Return to last particle
                 i--
             }
         }
+        canvas.primary.sortChildren()
 
         //Decrease remainingTime of emmission if it has one
-        if(this.remainingTime !== undefined){
+        if (this.remainingTime !== undefined) {
             this.remainingTime -= dt;
         }
 
-        if (this.spawnedEnable && this.particles.length < this.maxParticles && (this.remainingTime === undefined || this.remainingTime > 0)){
+        if (this.spawnedEnable && this.particles.length < this.maxParticles && (this.remainingTime === undefined || this.remainingTime > 0)) {
             //Spawned new particles
-            let numberNewParticles = 1 + Math.floor(this.spawningNumber * dt/this.particleFrequence)
-            let increaseTime = (this.spawningNumber * dt)%this.particleFrequence
+            let numberNewParticles = 1 + Math.floor(this.spawningNumber * dt / this.particleFrequence)
+            let increaseTime = (this.spawningNumber * dt) % this.particleFrequence
 
             //Don t overload the server during low framerate
-            if(numberNewParticles * 10 > this.maxParticles){
+            if (numberNewParticles * 10 > this.maxParticles) {
                 numberNewParticles = Math.floor(this.maxParticles / 10) + 1
                 increaseTime = 0;
             }
 
-            for(let i = 0; i < numberNewParticles; i++){
+            for (let i = 0; i < numberNewParticles; i++) {
                 const particle = this.particleTemplate.generateParticles(this.particleTemplate);
 
-                if(particle === undefined) {
+                if (particle === undefined) {
                     this.remainingTime = 0
                     break
                 }
                 ParticlesEmitter._EMISSION_CANVAS.addChild(particle.sprite);
+                canvas.primary.addChild(particle.sprite)
                 this.particles.push(particle)
             }
 
@@ -87,24 +89,26 @@ export default class ParticlesEmitter {
 
             setTimeout(this.enableSpawning.bind(this), this.particleFrequence + increaseTime)
 
-        }  if (this.remainingTime !== undefined && this.remainingTime <= 0 && this.particles.length === 0){
+        }
+
+        if (this.remainingTime !== undefined && this.remainingTime <= 0 && this.particles.length === 0) {
             //delete emission
             canvas.app.ticker.remove(this.callback);
-            const emitterIndex =  ParticlesEmitter.emitters.findIndex((emitter) => emitter.id === this.id)
+            const emitterIndex = ParticlesEmitter.emitters.findIndex((emitter) => emitter.id === this.id)
             ParticlesEmitter.emitters.splice(emitterIndex, 1)
         }
     }
 
     //Delete immediatly emission without waiting for each particle's end
-    _immediatelyStopEmission(){
-        while(this.particles.length > 0){
+    _immediatelyStopEmission() {
+        while (this.particles.length > 0) {
             let particle = this.particles[0]
             particle.sprite.destroy()
             this.particles.splice(0, 1)
         }
 
         canvas.app.ticker.remove(this.callback);
-        const emitterIndex =  ParticlesEmitter.emitters.findIndex((emitter) => emitter.id === this.id)
+        const emitterIndex = ParticlesEmitter.emitters.findIndex((emitter) => emitter.id === this.id)
         ParticlesEmitter.emitters.splice(emitterIndex, 1)
     }
 
