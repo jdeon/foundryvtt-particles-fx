56c1f9d7853f5c18f804c257ae064cc6ff15d971
jdeon <jd.eymann@gmail.com>
2024-10-21 00:46:18 +0200

    fix: particle above all if elevation not manage

--- script/object/particlesEmitter.js
+++ script/object/particlesEmitter.js
@@ -1,5 +1,6 @@
 import { motionTemplateDictionnary } from "../prefillMotionTemplate.js"
 import { colorTemplateDictionnary } from "../prefillColorTemplate.js"
+import { Particle } from "./particle.js"
 
 export default class ParticlesEmitter {
 
@@ -15,9 +16,10 @@ export default class ParticlesEmitter {
             canvas.app.stage.rendered.interface.moduleParticlesFx.destroy()
         }
 
-        const particleFxCanvas = new PIXI.Container()
+        const particleFxCanvas = new PIXI.Container();
+        particleFxCanvas.zIndex = Particle.SORT_LAYER;
         canvas.app.stage.rendered.interface.addChild(particleFxCanvas);
-        canvas.app.stage.rendered.interface.moduleParticlesFx = particleFxCanvas
+        canvas.app.stage.rendered.interface.moduleParticlesFx = particleFxCanvas;
         ParticlesEmitter._EMISSION_CANVAS = particleFxCanvas
     }
 
@@ -55,7 +57,10 @@ export default class ParticlesEmitter {
                 i--
             }
         }
-        canvas.primary.sortChildren()
+
+        if (this.particleTemplate?.isElevationManage) {
+            canvas.primary.sortChildren()
+        }
 
         //Decrease remainingTime of emmission if it has one
         if (this.remainingTime !== undefined) {
@@ -80,8 +85,11 @@ export default class ParticlesEmitter {
                     this.remainingTime = 0
                     break
                 }
+
                 ParticlesEmitter._EMISSION_CANVAS.addChild(particle.sprite);
-                canvas.primary.addChild(particle.sprite)
+                if (this.particleTemplate?.isElevationManage) {
+                    canvas.primary.addChild(particle.sprite)
+                }
                 this.particles.push(particle)
             }
 
